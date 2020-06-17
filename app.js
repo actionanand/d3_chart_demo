43b2a1008ff4b7cf10293b693177e70bf23b76ce
actionanand <meetanandns@gmail.com>
2020-06-18 00:58:08 +0530

    Working bar chart

--- app.js
+++ app.js
@@ -6,4 +6,46 @@ const dummy_data = [
     { id: 'd5', value: 3, name: 'Sri Lanka'}
 ];
 
-console.log(d3);
+const MARGIN = {
+    top: 20,
+    bottom: 10
+};
+const CHART_WIDTH = 600;
+const CHART_HEIGHT = 400 - MARGIN.top - MARGIN.bottom;
+
+const x = d3.scaleBand().rangeRound([0, CHART_WIDTH]).padding(0.1);
+const y = d3.scaleLinear().range([CHART_HEIGHT, 0]);
+
+const chartContainer = d3.select('svg')
+    .attr('width', CHART_WIDTH)
+    .attr('height', CHART_HEIGHT + MARGIN.top + MARGIN.bottom);
+
+    x.domain(dummy_data.map((data) => data.name));
+    y.domain([0, d3.max(dummy_data, d => d.value) + 3]);
+
+const chart = chartContainer.append('g');
+
+chart.append('g')
+    .call(d3.axisBottom(x).tickSizeOuter(0))
+    .attr('transform', `translate(0, ${CHART_HEIGHT})`)
+    .attr('color', 'forestgreen');
+
+chart.selectAll('.bar')
+    .data(dummy_data)
+    .enter()
+    .append('rect')
+    .classed('bar', true)
+    .attr('width', x.bandwidth())
+    .attr('height', (data) => CHART_HEIGHT - y(data.value))
+    .attr('x', (data) => x(data.name))
+    .attr('y', (data) => y(data.value));
+
+chart.selectAll('.label')
+    .data(dummy_data)
+    .enter()
+    .append('text')
+    .text((data) => data.value)
+    .attr('x', data => x(data.name) + x.bandwidth()/2)
+    .attr('y', data => y(data.value) - 10)
+    .attr('text-anchor', 'middle')
+    .classed('label', true);
