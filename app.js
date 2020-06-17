8daf450f501e23227cbe413fabf941ccc22cff15
actionanand <meetanandns@gmail.com>
2020-06-17 23:43:01 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const dummy_data = [
+    { id: 'd1', value: 13, name: 'India'},
+    { id: 'd2', value: 10, name: 'England'},
+    { id: 'd3', value: 17, name: 'USA'},
+    { id: 'd4', value: 7, name: 'Germany'},
+    { id: 'd5', value: 3, name: 'Sri Lanka'}
+];
